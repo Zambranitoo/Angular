df5858655a8bc2928423be73278d21eb5c2ad9d4
Felipe Zambrano <FelipeZambrano@protonmail.com>
2025-08-03 02:34:22 -0400

    Exercise project as an example

---
+++ example1/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { allIcons } from 'ngx-bootstrap-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'example1';
+}
---
+++ example1/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxBootstrapIconsModule.pick(allIcons)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
